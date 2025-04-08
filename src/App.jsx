9123785009a61d2e8234d66bcad8f1b83155e1bd
jayteemoney <jethroirmiya@gmail.com>
2025-04-08 20:01:08 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
