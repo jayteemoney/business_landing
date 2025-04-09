0d53710b419ff45ac6ece3e8c9fa997b8cb96def
jayteemoney <jethroirmiya@gmail.com>
2025-04-09 02:37:42 +0000

    stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
+import Stats from "./components/Feature";
 function App() {
   return (
     <Router> {/* ✅ Wrap everything inside BrowserRouter */}
       <Navbar />
       <Hero />
+      <Stats />
     </Router>
   );
 }
