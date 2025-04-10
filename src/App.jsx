2db1806be56a9c313c8c0086df6afd713dcea587
jayteemoney <jethroirmiya@gmail.com>
2025-04-10 19:14:39 +0000

    adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
-import {Navbar, Hero, Stats, Service, Billing, Card, Testimonial, Partners, Cta, Footer} from "./components"
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Service,
+  Billing,
+  Card,
+  Testimonial,
+  Partners,
+  Cta,
+  Footer,
+} from "./components";
+
 function App() {
   return (
-    <Router> 
-      <Navbar />
-      <Hero />
-      <Stats />
-      <Service/>
-      <Billing/>
-      <Card/>
-      <Testimonial />
-      <Partners />
-      <Cta />
-      <Footer />
+    <Router>
+      <div className="flex flex-col bg-[#00040F]">
+        <Navbar />
+        <div className="flex flex-col gap-12 md:gap-20 lg:gap-28">
+          <Hero />
+          <Stats />
+          <Service />
+          <Billing />
+          <Card />
+          <Testimonial />
+          <Partners />
+          <Cta />
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
