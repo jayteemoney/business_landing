99da80a6e765fbbd1c6d195081fec1953ec2d52c
jayteemoney <jethroirmiya@gmail.com>
2025-04-10 22:05:15 +0000

    adj

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,16 +5,16 @@ export default function Partners() {
 
   return (
     <section className=" bg-[#00040F] px-6 md:px-16 py-4">
-      <div className="max-w-6xl mx-auto flex flex-wrap justify-center gap-8 items-center">
-        {partners.map((logo, index) => (
-          <img
-            key={index}
-            src={logo}
-            alt={`Partner ${index + 1}`}
-            className="h-10 sm:h-12 opacity-60 hover:opacity-100 transition"
-          />
-        ))}
-      </div>
+   <div className="flex justify-center items-center gap-8 flex-wrap">
+    {partners.map((partner, index) => (
+      <img
+        key={index}
+        src={partner.src}
+        alt={`partner-${index}`}
+        className="h-10 w-auto object-contain"
+      />
+    ))}
+  </div>
     </section>
   );
 }
